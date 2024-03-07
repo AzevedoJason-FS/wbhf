298f28b57028cf065715fe2d7b89a69251b9a072
Jason Azevedo <JCAzevedo@student.fullsail.edu>
2024-03-07 16:07:41 -0500

    styling

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -4,38 +4,33 @@ const Header = () => {
   const date = new Date();
   return (
     <header>
-      <div className="header_1">
-        
-        <img src="/global-images/wbhf-logo.svg" alt="WBHF Logo" style={{ marginBottom: "2px" }} />
-
-      </div>
       <nav>
-        <div style={{maxWidth: '1400px',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center'}}>
-        <p style={{ textAlign: "start", color: 'white', width: '175px' }}>EST 1946</p>
-        <div className="nav-link-container">
-          <Link to={`/`} className="nav-link">
-            News
-          </Link>
-          <Link to={`/`} className="nav-link">
-            Station
-          </Link>
-          <Link to={`/`} className="nav-link">
-            Programming
-          </Link>
-          <Link to={`/`} className="nav-link">
-            Contact
-          </Link>
+        <div style={{ maxWidth: "1400px", width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center", padding: '0 20px', boxSizing: 'border-box' }}>
+          
+          <div className="nav-link-container">
+            <Link to={`/`} className="nav-link">
+              News
+            </Link>
+            <Link to={`/`} className="nav-link">
+              Station
+            </Link>
+            <Link to={`/`} className="nav-link">
+              Programming
+            </Link>
+            <Link to={`/`} className="nav-link">
+              Contact
+            </Link>
+          </div>
+
         </div>
-        <p style={{ textAlign: "end", color: 'white' }}>
-          {new Date(date).toLocaleDateString("en-us", { weekday: "long", day: "numeric", month: "long", year: "numeric" })}
-        </p>
-        </div>
-   
       </nav>
+      <div className="header_1">
+      <p style={{ textAlign: "start", width: '175px' }}>EST 1946</p>
+        <img src="/global-images/wbhf-logo.svg" alt="WBHF Logo" style={{ width: '264px'}} />
+        <p style={{ textAlign: "end", }}>
+            {new Date(date).toLocaleDateString("en-us", { weekday: "long", day: "numeric", month: "long", year: "numeric" })}
+          </p>
+      </div>
     </header>
   );
 };
